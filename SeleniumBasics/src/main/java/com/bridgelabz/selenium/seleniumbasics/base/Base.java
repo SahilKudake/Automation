be797acc5dcbb21ff8dc51277f821913a79720d8
Admin <sahilkudake7@gmail.com>
2020-03-02 13:17:14 +0530

    code updated

---
+++ SeleniumBasics/src/main/java/com/bridgelabz/selenium/seleniumbasics/base/Base.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.selenium.seleniumbasics.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base {
+
+	public static WebDriver driver;
+	
+	public Base() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+}
---
+++ SeleniumBasics/src/main/java/com/bridgelabz/selenium/seleniumbasics/wait/Implicit.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.selenium.seleniumbasics.wait;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+
+import com.bridgelabz.selenium.seleniumbasics.base.Base;
+
+public class Implicit extends Base {
+
+	static Base base;
+	public static void main(String[] args) throws InterruptedException {
+		base = new Base();
+		driver.get("https://www.google.com");
+		
+		driver.findElement(By.name("q")).sendKeys("Waited for 10 seconds");
+		Thread.sleep(1000);
+		driver.findElement(By.className("RNkmpXc")).click();;
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+}
